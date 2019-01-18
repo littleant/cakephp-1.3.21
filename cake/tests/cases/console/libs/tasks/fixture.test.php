eac967424a62eaf5103369b7023bea11f8569766
Andreas K <cakephp7@anty.at>
2019-01-18 12:03:57 +0100

    Add user fixture to FixtureTaskTest

--- cake/tests/cases/console/libs/tasks/fixture.test.php
+++ cake/tests/cases/console/libs/tasks/fixture.test.php
@@ -62,7 +62,7 @@ class FixtureTaskTest extends CakeTestCase {
  * @var array
  * @access public
  */
-	var $fixtures = array('core.article', 'core.comment', 'core.datatype', 'core.binary_test');
+	var $fixtures = array('core.article', 'core.comment', 'core.datatype', 'core.binary_test', 'core.user');
 
 /**
  * startTest method
